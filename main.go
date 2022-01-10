e1ea20eb968ad07bce58d93570c9ae1957c66575
Jeremy Bumsted <jeremy.bumsted@gmail.com>
2022-01-10 12:08:14 -0800

    logging the body again

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -19,14 +20,14 @@ func main() {
 	r.POST("/post", func(c *gin.Context) {
 		b := new(bytes.Buffer)
 		b.ReadFrom(c.Request.Body)
-		// rb, err := json.MarshalIndent(b.String(), "", "")
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
+		rb, err := json.MarshalIndent(b.String(), " ", " ")
+		if err != nil {
+			log.Fatal(err)
+		}
 		rcl := c.Request.Header.Get("Content-Length")
 		//rct := c.Request.Header.Get("Content-Type")
 
-		fmt.Printf("Content Length: %s\n", rcl)
+		fmt.Printf("Content Length: %s\n and the body\n%d", rcl, rb)
 	})
 
 	r.Run(":" + port)
